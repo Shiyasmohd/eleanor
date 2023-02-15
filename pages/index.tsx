d1af872ab74c33d2fe96c397ae65173c80f6dd71
shiyasmohd <shiyasjaseena124@gmail.com>
2023-02-15 22:05:20 +0530

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,3 @@
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import { useAccount } from 'wagmi'
 import Home from '../components/Home/Home'
 import Login from '../components/Login/LoginMainPage'
 
